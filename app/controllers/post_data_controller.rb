d0ba675b35362720267acde04d3622f976126dd7
Bryan <briverac@outlook.com>
2015-02-11 16:35:18 -0600

    Mejoré el funcionamiento y comenté el código

--- app/controllers/post_data_controller.rb
+++ app/controllers/post_data_controller.rb
@@ -1,18 +1,33 @@
 class PostDataController < ApplicationController
+
+  #Metodo que se llama para guardar los datos del sensor
   def create
 
+    #si se reciben los datos
+    if !(params[:post_datum].to_s == "{}")
+      #Se escriben los datos en el log
+      logger.info ''
+      logger.info 'DATOS RECIBIDOS'
+      logger.info ''
+      logger.info params[:post_datum]
+      logger.info ''
 
-    logger.info ''
-    logger.info 'DATOS RECIBIDOS'
-    logger.info ''
-    logger.info params
-    logger.info ''
+      render :json => {
+           :success => true,
+           :message => 'Data collected'
+       }, :status => 200
+      return
 
-    render :json => {
-         :success => true,
-         :message => "Data collected"
-     }, :status => 200
-    return
+    #Si no se reciben datos se notifica
+    else
+      logger.info ''
+      logger.info 'NO LLEGARON DATOS'
+      logger.info ''
+      render :json => {
+          :success => false,
+          :message => 'No data collected'
+      }, :status => 422
+    end
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  #Ruta del api
   post '/postdata', to: 'post_data#create'
 end
