645381ec7c3f7278a6b60d0c9dd257e8ddc527ca
Bryan <briverac@outlook.com>
2015-02-11 16:04:21 -0600

    Cree el controlador que procesa los datos y elimine una seguridad innecesaria

--- app/controllers/post_data_controller.rb
+++ app/controllers/post_data_controller.rb
@@ -1,2 +1,18 @@
 class PostDataController < ApplicationController
+  def create
+
+
+    logger.info ''
+    logger.info 'DATOS RECIBIDOS'
+    logger.info ''
+    logger.info params
+    puts ''
+
+    render :json => {
+         :success => true,
+         :message => "Data collected"
+     }, :status => 422
+    return
+
+  end
 end
