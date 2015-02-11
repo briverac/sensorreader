aaa035a67d274661b53c05b7df1f064ec4d9851a
Bryan <briverac@outlook.com>
2015-02-11 17:04:48 -0600

    Agregue la configuración del logger donde debe ir

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-#configuracion del logger
-config.logger = Logger.new(STDOUT)
-config.log_level = :info
